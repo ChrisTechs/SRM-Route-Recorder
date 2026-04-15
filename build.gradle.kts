e5ba4569d748a969614c9573ced4fb6c5730c9be
Chrisitian Steenkamp <christiannsteenkamp@gmail.com>
2026-04-16 01:33:33 +0200

    Make jar 3x smaller with Proguard

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+import proguard.gradle.ProGuardTask
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.guardsquare:proguard-gradle:7.9.1")
+    }
+}
+
 plugins {
     id("java")
     id("com.gradleup.shadow") version "8.3.0"
@@ -13,17 +24,42 @@ repositories {
 dependencies {
     implementation("net.minestom:minestom:2026.03.25-1.21.11")
     implementation("dev.hollowcube:polar:1.15.0")
-    implementation("it.unimi.dsi:fastutil:8.5.12")
+    implementation("it.unimi.dsi:fastutil:8.5.18")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
 }
 
 tasks {
     shadowJar {
+        archiveClassifier.set("all")
+        entryCompression = ZipEntryCompression.DEFLATED
+
         manifest {
             attributes("Main-Class" to "io.github.christechs.routerec.RouteRecorderServer")
         }
     }
 
-    build {
+    register<ProGuardTask>("minifyJar") {
         dependsOn(shadowJar)
+
+        injars(shadowJar.get().archiveFile)
+        outjars(layout.buildDirectory.file("libs/${project.name}-${project.version}-minified.jar"))
+
+        val javaHome = System.getProperty("java.home")
+        listOf("java.base", "java.logging", "java.desktop", "java.management", "jdk.jfr", "jdk.unsupported").forEach { mod ->
+            val filter = if (mod == "java.base") mapOf("jarfilter" to "!**.jar", "filter" to "!module-info.class") else emptyMap<String, String>()
+            libraryjars(filter, "$javaHome/jmods/$mod.jmod")
+        }
+
+        configuration("proguard-rules.pro")
+        verbose()
+    }
+
+    build {
+        dependsOn("minifyJar")
     }
 }
